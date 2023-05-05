3865b49fc9bd95410ebeef2bdd5b9a097d3ba77f
Joseph Cui <joeychui@icloud.com>
2023-05-05 18:27:17 +0000

    test

---
+++ leetcode-problems/src/main/java/me/joeychui/speedup/Test.java
@@ -0,0 +1,15 @@
+package me.joeychui.speedup;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 3, 2};
+
+        System.out.println(String.join("", arr));
+    }
+
+
+
+}
